c244316c866fbb3f6c50ffb9d42ff09cb21d93b3
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-08-25 05:29:44 +0800

    create section objects

--- src/scripts/sections.js
+++ src/scripts/sections.js
@@ -1,16 +1,25 @@
-class ContactsList {
-  constructor (label, type) {
+class ResumeSections {
+  constructor (label) {
     this.id = crypto.randomUUID();
     this.label = label;
-    this.type = type;
+    this.identifier = `${this.label.trim().replace(/\s+/g, '-').toLowerCase()}-section`;
+    this.visible = false;
   }
 }
 
-export const contactsList = [
-  new ContactsList('Email', 'email'),
-  new ContactsList('Phone Number', 'text'),
-  new ContactsList('LinkedIn', 'text'),
-  new ContactsList('X', 'text'),
-  new ContactsList('Facebook', 'text'),
-  new ContactsList('Instagram', 'text')
-]
+// IIFE to create ResumeSections instances.
+// To create additional sections, just hard-code it in "sections" variable.
+export const resumeSectionList = [];
+(function () {
+  const sections = [
+    'Personal Information',
+    'Professional Experience',
+    'Education',
+    'Skills',
+    'Projects'
+  ]
+
+  for (const section of sections) {
+    resumeSectionList.push(new ResumeSections(section));
+  }
+})();
