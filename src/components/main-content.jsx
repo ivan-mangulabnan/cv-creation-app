ae3c17567975be947a1d77647ebf394dd7b19d55
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-08-26 16:59:18 +0800

    create maincontent component and form state

--- src/components/main-content.jsx
+++ src/components/main-content.jsx
@@ -1,12 +1,19 @@
 import '../styles/main-contents.css';
 import { ResumeForms } from './resume-forms.jsx';
 import { ResumeDisplay } from './resume-display.jsx';
+import { useState } from 'react';
 
 export function MainContent () {
+  const [formData, setFormData] = useState({});
+
+  function handleChange (fieldName, value) {
+    setFormData(prev => ({...prev, [fieldName]: value}));
+  }
+
   return (
     <div className="main-content">
-      <ResumeForms />
-      <ResumeDisplay />
+      <ResumeForms formData={formData} onChange={handleChange}/>
+      <ResumeDisplay formData={formData}/>
     </div>
   )
 }
