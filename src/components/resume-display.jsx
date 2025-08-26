d1d3b0d7a060e55e8249bc69ee066653a0c76df7
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-08-26 17:01:07 +0800

    create personal information section in resume-display

--- src/components/resume-display.jsx
+++ src/components/resume-display.jsx
@@ -1,7 +1,42 @@
 import '../styles/resume-display.css';
+import { AddressSVG, PhoneSVG, EmailSVG } from './utils.jsx';
 
-export function ResumeDisplay () {
+export function ResumeDisplay ({formData}) {
   return (
-    <div className="resume-display"></div>
+    <div className="resume-display">
+      <div className='sheet'>
+        <PersonalInformationSection formData={formData}/>
+      </div>
+    </div>
+  )
+}
+
+const PersonalInformationSection = ({formData}) => {
+  const sections = [
+    { wrapperClass: 'address-section', value: formData.address, svg: AddressSVG, svgClass: 'address-svg' },
+    { wrapperClass: 'phone-section', value: formData['phone-number'], svg: PhoneSVG, svgClass: 'phone-svg' },
+    { wrapperClass: 'email-section', value: formData.email, svg: EmailSVG, svgClass: 'email-svg' }
+  ];
+
+  return (
+    <section>
+      <h1>{formData.fullname}</h1>
+      <p>{formData.occupation}</p>
+      <div className='contacts-div'>
+        { sections
+          .filter(section => section.value?.trim())
+          .map(section => <ContactSection key={section.wrapperClass} wrapperClass={section.wrapperClass} value={section.value} Svg={section.svg} svgClass={section.svgClass}/>)
+        }
+      </div>
+    </section>
+  )
+}
+
+function ContactSection ({wrapperClass, value, Svg, svgClass}) {
+  return (
+    <div className={`${wrapperClass}`}>
+      <Svg classname={svgClass}/>
+      <p>{value}</p>
+    </div>
   )
 }
